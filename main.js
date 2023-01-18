07a98836d093646d24d02065de0efcd94ef297a9
jungpa <operland55@naver.com>
2023-01-18 17:12:39 +0900

    ✨ main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const $navbar = document.querySelector("#navbar");
 const $navbarHeight = $navbar.getBoundingClientRect().height;
 const $navbarMenu = document.querySelector(".navbar__menu");
+const $navbarList = document.querySelectorAll(".navbar__menu__item");
+const projects = document.querySelectorAll(".project__content");
 
 // 메뉴에 맞춰 이동
 $navbarMenu.addEventListener("click", (e) => {
@@ -12,6 +14,9 @@ $navbarMenu.addEventListener("click", (e) => {
 	const scrollTo = document.querySelector(`.${link}`);
 
 	scrollTo.scrollIntoView({ behavior: "smooth" });
+	const active = document.querySelector(".navbar__menu__item.active");
+	active.classList.remove("active");
+	e.target.classList.add("active");
 });
 
 // header scroll 하면 bg색 바꾸기
@@ -21,5 +26,16 @@ window.addEventListener("scroll", () => {
 	} else {
 		$navbar.classList.remove("active");
 	}
-	console.log(window.scrollY);
 });
+
+let callback = (entries, observer) => {
+	entries.forEach((item) => {
+		if (item.isIntersecting) {
+			item.target.style.right = "0";
+		}
+	});
+};
+
+const observer = new IntersectionObserver(callback);
+
+projects.forEach((project) => observer.observe(project));
